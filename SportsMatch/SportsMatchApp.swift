61c89d689e7f2e3264239bb820a008fde77a3fef
Enzo Gallo <enzo@footbar.com>
2025-10-09 18:15:18 +0200

    Initial Commit

---
+++ SportsMatch/SportsMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsMatchApp.swift
+//  SportsMatch
+//
+//  Created by Enzo Gallo on 09/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SportsMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
